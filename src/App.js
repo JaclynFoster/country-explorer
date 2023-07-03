aff7519e27a0106828d0b050914a3379a6d98049
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-07-03 12:48:57 -0500

    completed part 2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import "./App.css";
 import Header from "./Components/Header";
 import MainDisplay from "./Components/MainDisplay";
 import OptionDisplay from "./Components/OptionDisplay";
+import { selectPotentials } from "./redux/slices/potentialCountriesSlice";
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
+
 
 function App() {
+    let potentials = useSelector(selectPotentials)
+    console.log(potentials)
     return (
         <div className="App font-link">
             <Header />
